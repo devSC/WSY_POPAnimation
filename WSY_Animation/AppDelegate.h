b3e030110bf30c511c7ba72dc14c65b89170bd1f
wilson-yuan <xiaochong2154@163.com>
2015-01-12 21:36:54 +0800

    initialize

---
+++ WSY_Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSY_Animation
+//
+//  Created by 袁仕崇 on 15/1/12.
+//  Copyright (c) 2015年 wilson-yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSY_Animation/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  WSY_Animation
+//
+//  Created by 袁仕崇 on 15/1/12.
+//  Copyright (c) 2015年 wilson-yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
